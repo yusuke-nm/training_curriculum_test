99e2c6359928a30c5f16844a48da0a471afd374a
you1107exp <royal.code0028@gmail.com>
2020-07-31 15:16:32 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,8 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    @plan = Plan.find(params[:id])
+    @plan = @plan.create(plan_params)
     redirect_to action: :index
   end
 
